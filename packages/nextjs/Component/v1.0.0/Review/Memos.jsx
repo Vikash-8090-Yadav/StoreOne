910f6004039e07a178eae9abbb0ccb3b00367d04
Beast <beastthermal@gmail.com>
2024-07-01 02:22:43 +0530

    Added the Subgraph

--- packages/nextjs/Component/v1.0.0/Review/Memos.jsx
+++ packages/nextjs/Component/v1.0.0/Review/Memos.jsx
@@ -4,190 +4,133 @@
 import { useState, useEffect } from "react";
 import { createClient } from "urql";
 
-import { notification } from 'antd';
-import Navbar from "../../Course/Nav";
-async function Deal(cid){
-
-  try{
-  const response1 = await axios.get(`https://api.lighthouse.storage/api/lighthouse/get_proof?network=testnet&cid=${cid}`);
-    const dealInfo = response1.data.dealInfo[0].dealId;
-    const polygonScanlink = `https://calibration.filfox.info/en/deal/${dealInfo}`;
-
-    notification.success({
-      message: 'Click here for the verification',
-      description: (
-        <div>
-          Transaction Hash: <a href={`https://calibration.filfox.info/en/deal/${dealInfo}`} target="_blank" rel="noopener noreferrer">{`https://calibration.filfox.info/en/deal/${dealInfo}`}</a>
-        </div>
-      )
-    });
-
-  }
-  catch(error){
-
-    console.error('Error occurred:', error);
-    // Handle the error here, you can log it or show a toast message indicating the error.
-    // For example:
-    notification.error({
-      message: "Miners still verifying!",
-      description: (
-        <div>
-          
-        </div>
-      )
-    });
-  }
-
-
-}
-
-
-
-async function Verify(cid){
-
-
-  try {
-    // https://gateway.lighthouse.storage/ipfs/QmTMVyisen2BrnoHg1fyA53n6fnqETMvedcDRPsoNH5rN8
-    // const response1 = await axios.get(`https://api.lighthouse.storage/api/lighthouse/get_proof?network=testnet&cid=${cid}`);
-    // const dealInfo = response1.data.dealInfo[0].dealId;
-    // const polygonScanlink = `https://calibration.filfox.info/en/deal/${dealInfo}`;
-    
-
-    const verifyData = ` https://gateway.lighthouse.storage/ipfs/${cid}`;
-    notification.success({
-      message: ' Click here for the verification',
-      description: (
-        <div  >
-          Transaction Hash: <a href={`https://gateway.lighthouse.storage/ipfs/${cid}`} target="_blank" rel="noopener noreferrer">{cid}</a>
-        </div>
-      )
-    });
-  } catch (error) {
-    console.error('Error occurred:', error);
-    // Handle the error here, you can log it or show a toast message indicating the error.
-    // For example:
-    notification.error({
-      message: "Miners still verifying!",
-      description: (
-        <div>
-          
-        </div>
-      )
-    });
-    
-  }
-}
-
-
-
 const Memos = ({ state }) => {
   const [tokens, setTokens] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // Add loading state
 
-  const [memos, setMemos] = useState([]);
-  const { contract } = state;
+  const QueryURL = "https://api.studio.thegraph.com/query/67475/monetizedo/v0.0.1";
+
+  const query = `
+    {
+      donations(first: 10, orderBy: id) {
+      from
+      id
+      message
+      name
+      timestamp
+      }
+    }
+  `;
+
+  const client = createClient({
+    url: QueryURL
+  });
 
   useEffect(() => {
-    const memosMessage = async () => {
-      const memos = await contract.getMemos();
-      setMemos(memos);
+    if (!client) {
+      return;
+    }
+
+    const getTokens = async () => {
+      try {
+        const { data } = await client.query(query).toPromise();
+        setTokens(data.donations);
+        setIsLoading(false); // Data is loaded
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-    contract && memosMessage();
-  }, [contract]);
+
+    getTokens();
+  }, [client]);
+
+  // const { contract } = state;
+
+  // useEffect(() => {
+  //   const memosMessage = async () => {
+  //     if (contract) {
+  //       const memos = await contract.getMemos();
+  //       setMemos(memos);
+  //     }
+  //   };
+  //   memosMessage();
+  // }, [contract]);
 
   return (
     <>
-        <p style={{ textAlign: "center", marginTop: "20px" }}>Messages</p>
-      {memos.map((memo) => {
-        return (
-          <div
-            className="container-fluid"
-            style={{ width: "100%" }}
-            key={Math.random()}
-          >
-            <table
-              style={{
-                marginBottom: "10px",
-              }}
-            >
-              <tbody>
-                <tr>
-                  <td
-                    style={{
-                      backgroundColor: "#96D4D4",
-                      border: "1px solid white",
-                      borderCollapse: "collapse",
-                      padding: "7px",
-                      width: "100px",
-                    }}
-                  >
-                    {memo.name}
-                  </td>
-                  <td
-                    style={{
-                      backgroundColor: "#96D4D4",
-                      border: "1px solid white",
-                      borderCollapse: "collapse",
-                      padding: "7px",
-                      width: "800px",
-                    }}
-                  >
-                    {new Date(memo.timestamp * 1000).toLocaleString()}
-                  </td>
-                  <td
-                    style={{
-                      backgroundColor: "#96D4D4",
-                      border: "1px solid white",
-                      borderCollapse: "collapse",
-                      padding: "7px",
-                      width: "300px",
-                    }}
-                  >
-                    {memo.message}
-                  </td>
-                  <td
-                    style={{
-                      backgroundColor: "#96D4D4",
-                      border: "1px solid white",
-                      borderCollapse: "collapse",
-                      padding: "7px",
-                      width: "400px",
-                    }}
-                  >
-                    {memo.from}
-                  </td>
-                  
-                  <td
-                    style={{
-                      backgroundColor: "#96D4D4",
-                      border: "1px solid white",
-                      borderCollapse: "collapse",
-                      padding: "7px",
-                      width: "400px",
-                    }}
-                  >
-                   <button className=" hover:rotate-2 delay-100 transition ease-in-out   text-center border hover:bg-gray-100 hover:shadow-md border-gray-500 rounded-md mt-4  bg-yellow-500 text-cyan font-bold py-3 px-12 mx-1 rounded" onClick={() => Verify(memo.cid)}>Verify</button>
-                
-                  </td>
-                  <td
-                    style={{
-                      backgroundColor: "#96D4D4",
-                      border: "1px solid white",
-                      borderCollapse: "collapse",
-                      padding: "7px",
-                      width: "400px",
-                    }}
-                  >
-
-                  <button className=" hover:rotate-2 delay-100 transition ease-in-out   text-center border hover:bg-gray-100 hover:shadow-md border-gray-500 rounded-md mt-4  bg-gray-500 text-cyan font-bold py-3 px-12 rounded" onClick={() => Deal(memo.cid)}>DEALID</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+      <p className="mncnt" style={{ textAlign: "center", marginTop: "20px" }}>
+        REVIEWS
+      </p>
+      {isLoading ? (
+        // Show loading indicator while data is being fetched
+        <div>Loading...</div>
+      ) : tokens.length > 0 ? (
+        tokens.map((token) => (
+          <div className="subcnt" key={token.id}>
+            <div className="container-fluid" style={{ width: "100%" }}>
+              <table
+                className="tb"
+                style={{
+                  marginBottom: "10px",
+                }}
+                responsive={true}
+              >
+                <tbody>
+                  <tr className="tr">
+                    <td
+                      style={{
+                        backgroundColor: "#96D4D4",
+                        border: "1px solid white",
+                        borderCollapse: "collapse",
+                        padding: "7px",
+                        width: "100px",
+                      }}
+                    >
+                      {token.name}
+                    </td>
+                    <td
+                      style={{
+                        backgroundColor: "#96D4D4",
+                        border: "1px solid white",
+                        borderCollapse: "collapse",
+                        padding: "7px",
+                        width: "800px",
+                      }}
+                    >
+                      {new Date(token.timestamp * 1000).toLocaleString()}
+                    </td>
+                    <td
+                      style={{
+                        backgroundColor: "#96D4D4",
+                        border: "1px solid white",
+                        borderCollapse: "collapse",
+                        padding: "7px",
+                        width: "300px",
+                      }}
+                    >
+                      {token.message}
+                    </td>
+                    <td
+                      style={{
+                        backgroundColor: "#96D4D4",
+                        border: "1px solid white",
+                        borderCollapse: "collapse",
+                        padding: "7px",
+                        width: "400px",
+                      }}
+                    >
+                      {token.from}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-        );
-      })}
-      </>
+        ))
+      ) : (
+        <div>No data available</div>
+      )}
+    </>
   );
 };
 
--- packages/nextjs/app/marketplace.js
+++ packages/nextjs/app/marketplace.js
@@ -1,150 +1,69 @@
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import {Web3} from 'web3';
+import Web3Modal from 'web3modal'
+import Navbar from "../Component/Course/Nav";
+import { createClient } from "urql";
+import { ToastContainer, toast } from 'react-toastify';
 import { notification } from 'antd';
+import {Web3} from 'web3';
 
-import Navbar from './Navbar';
 import {
   marketplaceAddress
 } from '../config'
 
-import NFTMarketplace from '../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json'
-
-
-async function Deal(cid){
-
-  try{
-  const response1 = await axios.get(`https://api.lighthouse.storage/api/lighthouse/get_proof?network=testnet&cid=${cid}`);
-    const dealInfo = response1.data.dealInfo[0].dealId;
-    const polygonScanlink = `https://calibration.filfox.info/en/deal/${dealInfo}`;
-
-    notification.success({
-      message: 'Click here for the verification',
-      description: (
-        <div>
-          Transaction Hash: <a href={`https://calibration.filfox.info/en/deal/${dealInfo}`} target="_blank" rel="noopener noreferrer">{`https://calibration.filfox.info/en/deal/${dealInfo}`}</a>
-        </div>
-      )
-    });
-
-  }
-  catch(error){
-
-    console.error('Error occurred:', error);
-    // Handle the error here, you can log it or show a toast message indicating the error.
-    // For example:
-    notification.error({
-      message: "Miners still verifying!",
-      description: (
-        <div>
-          
-        </div>
-      )
-    });
-  }
-
-
-}
+import NFTMarketplace from '../abi/marketplace.json'
 
 
 
-async function Verify(cid){
 
 
-  try {
-    // https://gateway.lighthouse.storage/ipfs/QmTMVyisen2BrnoHg1fyA53n6fnqETMvedcDRPsoNH5rN8
-    // const response1 = await axios.get(`https://api.lighthouse.storage/api/lighthouse/get_proof?network=testnet&cid=${cid}`);
-    // const dealInfo = response1.data.dealInfo[0].dealId;
-    // const polygonScanlink = `https://calibration.filfox.info/en/deal/${dealInfo}`;
-    
-
-    const verifyData = ` https://gateway.lighthouse.storage/ipfs/${cid}`;
-    notification.success({
-      message: ' Click here for the verification',
-      description: (
-        <div  >
-          Transaction Hash: <a href={`https://gateway.lighthouse.storage/ipfs/${cid}`} target="_blank" rel="noopener noreferrer">{cid}</a>
-        </div>
-      )
-    });
-  } catch (error) {
-    console.error('Error occurred:', error);
-    // Handle the error here, you can log it or show a toast message indicating the error.
-    // For example:
-    notification.error({
-      message: "Miners still verifying!",
-      description: (
-        <div>
-          
-        </div>
-      )
-    });
-    
-  }
-}
 
 
-async function Provider(cid){
-  notification.success({
-    message: ' The  Eligible and active storage provider is',
-    description: (
-      <div  >
-        t017840
-      </div>
-    )
-  });
-}
 
-
-async function PODSI(cid){
-  try {
-    // https://gateway.lighthouse.storage/ipfs/QmTMVyisen2BrnoHg1fyA53n6fnqETMvedcDRPsoNH5rN8
-    const response1 = await axios.get(`https://api.lighthouse.storage/api/lighthouse/get_proof?network=testnet&cid=${cid}`);
-    
-
-    
-    notification.success({
-      message: ' Click here for the verification',
-      description: (
-        <div  >
-          Transaction Hash: <a href={`https://api.lighthouse.storage/api/lighthouse/get_proof?network=testnet&cid=${cid}`} target="_blank" rel="noopener noreferrer">{'CLICK HERE'}</a>
-        </div>
-      )
-    });
-  } catch (error) {
-    console.error('Error occurred:', error);
-    // Handle the error here, you can log it or show a toast message indicating the error.
-    // For example:
-    notification.error({
-      message: "Miners still verifying!",
-      description: (
-        <div>
-          
-        </div>
-      )
-    });
-    
-  }
-}
-
-
-export default function Home1() {
- 
+export default function Home() {
   const [nfts, setNfts] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
+  const [isLoading, setIsLoading] = useState(true); 
+  const [tokens, setTokens] = useState([]);
+
+
+  
+  const QueryURL = "https://api.studio.thegraph.com/query/67475/monetizedo/v0.0.1";
+
+  let query = `
+    {
+      tokenItems {
+        newTokenId
+      tokenURI
+      price
+      transactionHash
+      }
+    }
+  `;
+
+  const client = createClient({
+    url: QueryURL
+  });
+
   useEffect(() => {
+
+    if (!client) {
+      return;
+    }
+
     async function  test(){
+
       const networks = {
-        calibrationnet: {
-        chainId: `0x${Number(314159).toString(16)}`,
-        chainName: "calibrationnet",
+        basesepolia: {
+        chainId: `0x${Number(84532).toString(16)}`,
+        chainName: "basesepolia",
         nativeCurrency: {
-          name: "calibrationnet",
-          symbol: "tFIL",
+          name: "basesepolia",
+          symbol: "ETH",
           decimals: 18,
         },
-        rpcUrls: ["https://api.calibration.node.glif.io/rpc/v1"],
+        rpcUrls: ["https://sepolia.base.or"],
       },
     };
     if(typeof window.ethereum =="undefined"){
@@ -152,11 +71,11 @@ export default function Home1() {
   }
   let web3 =  new Web3(window.ethereum);
  
-  if(web3.network !=="calibrationnet"){
+  if(web3.network !=="basesepolia"){
       await window.ethereum.request({
           method:"wallet_addEthereumChain",
           params:[{
-              ...networks["calibrationnet"]
+              ...networks["basesepolia"]
           }]
       })
   }
@@ -173,52 +92,52 @@ async function checkNetworkAndLoadNFTs() {
 
     let chk = localStorage.getItem("ChainId");
 
-    // if (chk !== '314159') { 
-    //   await test();
-    // }
+    const { data } = await client.query(query).toPromise();
+      setTokens(data.tokenItems);
+      console.log(data.tokenItems);
+      setIsLoading(false); // Data is loaded
+      await loadNFTs();
 
-    loadNFTs();
+    if (chk !== '84532') { 
+      await test();
+    }
+    
   } catch (error) {
     console.error('Error checking network or loading NFTs:', error);
   }
 }
 
 checkNetworkAndLoadNFTs();
-  }, [])
-  async function loadNFTs() {
-    /* create a generic provider and query for unsold market items */
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send('eth_requestAccounts', []);
-    const signer = provider.getSigner();
-    
-    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
-    const data = await contract.fetchMarketItems()
+  }, [client])
 
-    /*
-    *  map over items returned from smart contract and format 
-    *  them as well as fetch their token metadata
-    */
-    const items = await Promise.all(data.map(async i => {
-      const tokenUri = await contract.tokenURI(i.tokenId)
-      const meta = await axios.get(tokenUri)
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-      let item = {
+async function loadNFTs() {
+  // await test();
+  
+  try {
+
+    const items = await Promise.all(tokens.map(async token => {
+      // alert("try")
+      const meta = await axios.get(token.tokenURI);
+      console.log("The i item is".meta);
+      const price = await token.price / 10 ** 18;
+      const tokenId = await token.newTokenId;
+      // alert("inside try")
+      return {
         price,
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
+        tokenId,
         image: meta.data.image,
         name: meta.data.name,
         description: meta.data.description,
-        cid1:meta.data.cid1,
-      }
-      console.log(item)
-      return item
-    }))
-    console.log(items);
-    setNfts(items)
-    setLoadingState('loaded') 
+      };
+    }));
+    
+    setNfts(items);
+    setLoadingState('loaded');
+  } catch (error) {
+    console.error("Error loading NFTs:", error);
+    setLoadingState('error');
   }
+}
   async function buyNft(nft) {
     /* needs the user to sign the transaction, so will use Web3Provider and sign it */
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -236,20 +155,24 @@ checkNetworkAndLoadNFTs();
   }
   if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-white text-3xl">No Item in marketplace</h1>)
   return (
+    <>
+  
     <div className="flex mrkt  justify-center">
+      {/* <Navbar/> */}
       <div className="px-10" style={{ maxWidth: '1600px' }}>
         <div className="grid flex  grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-1 unmrk">
           {
             nfts.map((nft, i) => (
-              <div key={i} className="border rounded-t-md  umrkt shadow rounded-xl overflow-hidden">
+              <div key={i} className="border rounded-t-md   shadow rounded-xl overflow-hidden">
                 <img   height="25px"  className = " w-full rounded-t-md duration-200 hover:scale-110 hover:overflow-hidden" src={nft.image} />
                 <div className="p-1">
                   <p style={{ height: '100%' }} className="text-2xl font-semibold">{nft.name}</p>
                   <div style={{ height: '70px', overflow: 'hidden' }}>
                     <p className="text-gray-400">{nft.description}</p>
                   </div>
+                 
                 </div>
-                
+              
                 <div className="p-1  umrk bg-black">
                   <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
                   <button className=" hover:rotate-2 delay-100 transition ease-in-out   text-center border hover:bg-gray-100 hover:shadow-md border-gray-500 rounded-md mt-4 w-full bg-green-500 text-cyan font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
@@ -261,5 +184,5 @@ checkNetworkAndLoadNFTs();
         </div>
       </div>
     </div>
-  )
+    </>)
 }
